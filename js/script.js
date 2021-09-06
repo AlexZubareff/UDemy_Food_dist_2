e27d4efbc8d8c12deede9870de313de1fbc0dd28
Alex <azv99@yandex.ru>
2021-09-06 13:13:18 +0300

    Lesson 60. Библиотеки. Бблиотека Axios.

--- js/script.js
+++ js/script.js
@@ -192,11 +192,19 @@ modalTrigger.forEach(item => {
     };
 
 
-    getResource('http://localhost:3000/menu')
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => {
+    //         data.forEach(({img, altimg, title, descr, price}) => {
+    //             new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+    //         });
+    //     });
+
+
+    axios.get('http://localhost:3000/menu')
         .then(data => {
-            data.forEach(({img, altimg, title, descr, price}) => {
+            data.data.forEach(({img, altimg, title, descr, price}) => {
                 new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
-            });
+            }); 
         });
 
     // getResource('http://localhost:3000/menu')
